d48315549e87b534c7bbeedc107d69aa2938b040
Brad Campbell <bradjc5@gmail.com>
2014-11-13 15:07:28 -0500

    server for accessors

---
+++ server/accessor_host.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import copy
+import json
+import os
+
+import tornado.ioloop
+import tornado.web
+
+
+
+
+
+
+server_path_tuples = []
+
+
+# Base class for serving accessors.
+class ServeAccessor (tornado.web.RequestHandler):
+	def get (self):
+		self.set_header('Content-Type', 'application/json')
+
+		# See if any of the parameters should be configured
+		if 'parameters' in self.accessor:
+			for p in self.accessor['parameters']:
+				p['value'] = self.get_argument(p['name'], p['default'], True)
+
+		accessor_json = json.dumps(self.accessor, indent=4).replace('\\n', '\n')
+		self.write(accessor_json)
+
+
+def create_accessor (structure, ports, accessor):
+	# Combine all of the ports from the interfaces and the accessor itself
+	accessor['ports'] = ports + accessor['ports']
+
+	# Create the URL based on the hierarchy
+	name = ''.join(structure)
+	path = '/{}'.format('/'.join(structure))
+
+	# Create a class for the tornado webserver to use when the accessor
+	# is requested
+	serve_class = type('ServeAccessor' + name, (ServeAccessor,), {
+		'accessor': accessor
+	})
+
+	# Add the accessor to the list of valid accessors to request
+	server_path_tuples.append((path, serve_class))
+
+
+
+def find_accessors (path, structure, ports):
+
+	sub_structure = copy.deepcopy(structure)
+	sub_ports     = copy.deepcopy(ports)
+
+	# Get the name of the folder we are currently in
+	folder = os.path.basename(os.path.normpath(path))
+
+	# See if there is a .json file in this folder with the same
+	# name as the folder. If so, this is the interface file
+	interface_path = os.path.join(path, folder) + '.json'
+	if os.path.isfile(interface_path):
+		with open(interface_path) as f:
+			j = json.load(f, strict=False)
+			sub_structure += [folder]
+			if 'ports' in j:
+				sub_ports += j['ports']
+				print(sub_ports)
+
+
+	# Look for any other .json files. These are accessors
+	contents = os.listdir(path)
+	for item in contents:
+		item_path = os.path.join(path, item)
+		# Do only .json files on the first pass
+		if os.path.isfile(item_path):
+			filename, ext = os.path.splitext(os.path.basename(item_path))
+			if ext == '.json' and filename != folder:
+				# This must be an accessor file
+				with open(item_path) as f:
+					j = json.load(f, strict=False)
+					create_accessor(sub_structure+[filename], sub_ports, j)
+
+
+	# Do the directories
+	for item in contents:
+		item_path = os.path.join(path, item)
+		if os.path.isdir(item_path):
+			find_accessors(item_path, sub_structure, sub_ports)
+
+
+
+
+DESC = """
+Run an accessor hosting server.
+"""
+
+
+parser = argparse.ArgumentParser(description=DESC)
+parser.add_argument('-p', '--path',
+                    required=True,
+                    help='The root of the tree that holds the accessors.')
+args = parser.parse_args()
+
+
+find_accessors(args.path, [], [])
+
+print(server_path_tuples)
+
+
+
+t = tornado.web.Application(server_path_tuples)
+
+t.listen(6565)
+
+# Run the loop!
+# This works for both tornado and pika/rabbitmq
+tornado.ioloop.IOLoop.instance().start()
+
+
