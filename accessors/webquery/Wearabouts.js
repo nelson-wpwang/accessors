9782875680c1057844d30d8dc3877a49eac4f012
Brad Campbell <bradjc5@gmail.com>
2014-11-23 22:57:03 -0500

    added wearabouts

---
+++ accessors/webquery/Wearabouts.js
@@ -0,0 +1,30 @@
+function init () {
+	Update();
+}
+
+function Update () {
+	var pid = get_parameter('profile_id');
+	var gatd = get_parameter('gatd_url');
+	var query = btoa(JSON.stringify({'location_str':get_parameter('location')}));
+	var url = gatd + '/viewer/recent/'+pid+'?limit=1&query='+query;
+
+	data = JSON.parse(readURL(url));
+
+	if (data.length == 0) {
+		set('People', 'Nobody in the room');
+	} else {
+		people_list = data[0].person_list;
+		people_names = [];
+		for (var i=0; i<people_list.length; i++) {
+			person = people_list[i];
+			for (key in person) {
+				people_names.push(person[key]);
+			}
+		}
+		if (people_names.length == 0) {
+			set('People', 'Nobody in the room');
+		} else {
+			set('People', people_names.join(', '));
+		}
+	}
+}
