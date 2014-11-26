3a8f79aef9f24211ff1f57c75dcf100533ca82b3
Pat Pannuto <pat.pannuto@gmail.com>
2014-11-26 14:09:23 -0500

    Give better hint of expected websocket URL

--- runtimes/web/ws_server.py
+++ runtimes/web/ws_server.py
@@ -34,7 +34,7 @@ def handler(websocket, path):
 		#yield from websocket.send(greeting)
 
 start_server = websockets.serve(handler, None, 8765)
-logger.info("Websocket server listening on localhost:8765")
+logger.info("Websocket server listening on ws://localhost:8765")
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
