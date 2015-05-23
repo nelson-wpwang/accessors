6f373562573c22bd3e91238d0247c1500b43439e
Pat Pannuto <pat.pannuto@gmail.com>
2015-05-23 03:02:35 -0400

    Spaces to Tabs [sorry]

--- server/accessor_host.py
+++ server/accessor_host.py
@@ -817,49 +817,49 @@ def jinja_filter_markdown (string):
 
 # Helper classes for rendering jinja templates
 class JinjaTemplateRendering:
-    """
-    A simple class to hold methods for rendering templates.
-    """
-    def render_template (self, template_name, **kwargs):
-        template_dirs = []
-        if self.settings.get('template_path', ''):
-            template_dirs.append(
-                self.settings["template_path"]
-            )
+	"""
+	A simple class to hold methods for rendering templates.
+	"""
+	def render_template (self, template_name, **kwargs):
+		template_dirs = []
+		if self.settings.get('template_path', ''):
+			template_dirs.append(
+				self.settings["template_path"]
+			)
 
-        env = jinja2.Environment(
-                loader=jinja2.FileSystemLoader(template_dirs),
-                extensions=['jinja2.ext.i18n'],
-                )
-        env.filters['markdown'] = jinja_filter_markdown
-        env.install_null_translations()
+		env = jinja2.Environment(
+				loader=jinja2.FileSystemLoader(template_dirs),
+				extensions=['jinja2.ext.i18n'],
+				)
+		env.filters['markdown'] = jinja_filter_markdown
+		env.install_null_translations()
 
-        try:
-            template = env.get_template(template_name)
-        except jinja2.TemplateNotFound:
-            raise TemplateNotFound(template_name)
-        content = template.render(kwargs)
-        return content
+		try:
+			template = env.get_template(template_name)
+		except jinja2.TemplateNotFound:
+			raise TemplateNotFound(template_name)
+		content = template.render(kwargs)
+		return content
 
 class JinjaBaseHandler (tornado.web.RequestHandler, JinjaTemplateRendering):
-    """
-    RequestHandler already has a `render()` method. I'm writing another
-    method `renderj()` and keeping the API almost same.
-    """
-    def renderj (self, template_name, **kwargs):
-        """
-        This is for making some extra context variables available to
-        the template
-        """
-        kwargs.update({
-            'settings': self.settings,
-            'STATIC_URL': self.settings.get('static_url_prefix', '/static/'),
-            'request': self.request,
-            'xsrf_token': self.xsrf_token,
-            'xsrf_form_html': self.xsrf_form_html,
-        })
-        content = self.render_template(template_name, **kwargs)
-        self.write(content)
+	"""
+	RequestHandler already has a `render()` method. I'm writing another
+	method `renderj()` and keeping the API almost same.
+	"""
+	def renderj (self, template_name, **kwargs):
+		"""
+		This is for making some extra context variables available to
+		the template
+		"""
+		kwargs.update({
+			'settings': self.settings,
+			'STATIC_URL': self.settings.get('static_url_prefix', '/static/'),
+			'request': self.request,
+			'xsrf_token': self.xsrf_token,
+			'xsrf_form_html': self.xsrf_form_html,
+		})
+		content = self.render_template(template_name, **kwargs)
+		self.write(content)
 
 
 
