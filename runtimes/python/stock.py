35a3e848b65168bfd6e5f99e5c6f580b422fc5ac
Pat Pannuto <pat.pannuto@gmail.com>
2015-05-26 15:56:32 -0400

    Add detailed comments to stock example

--- runtimes/python/stock.py
+++ runtimes/python/stock.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
-import sh
-
 import accessors
 
-#stocktick = accessors.get_accessor_by_location('localhost:6565', 'Anywhere', 'StockTick')
+# First step is to get a "StockTick" object. This is an object that
+# encapsulates the accessor so that we can interact with it
 stocktick = accessors.get_accessor('/webquery/StockTick')
 
 for symbol in ['GOOG', 'MSFT', 'YHOO']:
+	# The StockTick accessor has two ports: "StockSymbol" and "Price".
+
+	# Accessor ports have associated directions: input, output, and observe.
+	# Assignments to a port map to input and reads from a port map to output.
+	# (Look bitcoin for an example of observe)
+
+	# To use the StockTick accessor, we first set the StockSymbol:
 	stocktick.StockSymbol = symbol
+
+	# The we read the Price of the stock:
 	print("Stock {} price {}".format(symbol, stocktick.Price))
